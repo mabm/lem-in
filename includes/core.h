5a1a14b4bd3f2c67aace21c144cfda3cad2727cb
Nicolas ades <nicolas.ades@epitech.eu>
2014-04-22 03:16:49 +0200

    nico

---
+++ includes/core.h
@@ -0,0 +1,20 @@
+/*
+** core.h for  in /home/nicolas/Workspace/lem_in/includes
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Tue Apr 22 03:12:44 2014 Nicolas Ades
+** Last update Tue Apr 22 03:16:15 2014 Nicolas Ades
+*/
+
+#ifndef CORE_H_
+# define CORE_H_
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include "my.h"
+
+#endif /* CORE_H_ */
